3ba9168c3ab08461af554600d24d56b9cee39acb
SamJian <koko0809@gmail.com>
2025-07-15 08:32:20 +0800

    更新測試與綠界界接

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use TsaiYiHua\ECPay\Checkout;
+
+class TestController extends Controller
+{
+
+    protected $checkout;
+
+    public function __construct(Checkout $checkout)
+    {
+        $this->checkout = $checkout;
+    }
+
+    public function ecpay(Request $request)
+    {
+
+
+        $formData = [
+            'UserId' => 1, // 用戶ID , Optional
+            'ItemDescription' => '產品簡介',
+            'ItemName' => '測試商品',
+            'TotalAmount' => '10',
+            'PaymentMethod' => 'Credit', // ALL, Credit, ATM, WebATM
+        ];
+        return $this->checkout->setReturnUrl("")->setPostData($formData)->send();
+    }
+
+    public function ecpayCallback(Request $request){
+        $post = $request->post();
+
+        dd($request , $post);
+    }
+
+}
